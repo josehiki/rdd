a1fa691f8919272921caf77e31bb50d9e998d356
Jose Morgado <josehiki@gmail.com>
2020-06-22 19:18:41 -0500

    CDB

--- app/models/db.php
+++ app/models/db.php
@@ -12,9 +12,9 @@
 		
 		function __construct()
 		{
-			$this->servername = "localhost"; 
+			$this->servername = "rdd-bd.cqtvq7d22yp2.us-east-2.rds.amazonaws.com"; 
 			$this->username = "root"; 
-			$this->password = ""; 
+			$this->password = "&tTgR8UA"; 
 			$this->nameDB = "rdd"; 
 		}
 	}
