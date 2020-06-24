4305c8c87fb5e84ca5d092a04a1de97ed84df18f
Jose Morgado <josehiki@gmail.com>
2020-06-24 00:30:06 -0500

    Creado: Pagina para cargar las preguntas del cuestionario

--- views/juego.php
+++ views/juego.php
@@ -8,17 +8,38 @@
     $tema = $_GET['tema'];
     $equipoRojo =  $_SESSION['equipoRojo'];
     $equipoAzul =  $_SESSION['equipoAzul'];
-    print_r($equipoRojo);
-    print_r($equipoAzul);
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Juego</title>
-</head>
-<body>
-    
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Juego</title>
+		<link rel="stylesheet" type="text/css" href="css/estilos.css"/>
+
+    </head>
+    <body onload="cargaPreguntas()">
+        <div class="encabezado">
+			<a class="gestionarPreguntas" href="../app/logoutController.php" >
+				Cerrar sesión
+			</a>
+		</div>
+    </body>
+    <?php
+        echo "<script>var temaJs='$tema'</script>";
+    ?>
+    <script type="text/javascript">
+
+        function cargaPreguntas()
+        {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                alert(this.responseText);
+            }
+            };
+            xmlhttp.open("GET", "../app/loadPreguntasJuego.php?tema="+temaJs, true);
+            xmlhttp.send();
+        }
+    </script>
 </html>
