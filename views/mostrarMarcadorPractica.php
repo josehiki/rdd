486342e0c9fe75c291d4129f53dc672226bdb553
Jose Morgado <josehiki@gmail.com>
2020-06-29 18:46:48 -0500

    Agregado: Estilos a la pantalla marcador en el juego de Practica

--- views/mostrarMarcadorPractica.php
+++ views/mostrarMarcadorPractica.php
@@ -8,20 +8,33 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Marcador</title>
+        <link rel="stylesheet" type="text/css" href="css/miscelanea.css"/>
 		<link rel="stylesheet" type="text/css" href="css/estilos.css"/>
+        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
     </head>
-    <body>
+    <body class="body-marcador">
         <div class="encabezado"></div>
-        <div>
-            <h1>¡¡En hora buena!!</h1>
-            <h4>Gran trabajo</h4>
-            <h2>Tu marcador es</h2>
+        <div class="contenedor-div contenedor-marcadorP">
+            <h1 class="titulos titulo-marcadorP"><span>¡¡</span> En hora buena <span>!!</span></h1>
+            <h3>Gran trabajo</h3>
+            <br>
             <?php
-                echo "<h2>$puntos</h2>";
+                echo "<h2>Tu marcador es<br>$puntos</h2>";
             ?>
         </div>
-        <div>
-            <a href="../">Pantalla Principal</a>
-        </div>
+        <div class="marcador-footer">
+            <a href="">
+                <i class='fas fa-redo-alt'></i>
+                Volver a jugar
+            </a>
+            <a href="">
+                <i class='fas fa-book'></i>
+                Cambiar tema
+            </a>
+            <a href="../">
+                <i class='fas fa-home'></i>
+                Pantalla Principal
+            </a>
+        </div>            
     </body>
 </html>
